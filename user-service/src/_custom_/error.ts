9c4b9375dfe8eb5749b8b1a4b44c901984e7683a
Kingsley Victor <javaprodigy56@gmail.com>
2020-07-14 18:51:04 -0400

    New commit

--- user-service/src/_custom_/error.ts
+++ user-service/src/_custom_/error.ts
@@ -1,7 +1,7 @@
 export class CustomError extends Error {
- public code$: number;
+ public code: number;
  constructor(message: string, code$: number) {
   super(message);
-  this.code$ = code$;
+  this.code = code$;
  }
 }
