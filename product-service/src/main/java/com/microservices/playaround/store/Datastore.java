d0496901cf8c5d4c23e4006e43bee35c64a50678
Kingsley Victor <javaprodigy56@gmail.com>
2020-07-16 16:16:48 -0400

    New commit

--- product-service/src/main/java/com/microservices/playaround/store/Datastore.java
+++ product-service/src/main/java/com/microservices/playaround/store/Datastore.java
@@ -1,8 +1,36 @@
 package com.microservices.playaround.store;
 
+import com.microservices.playaround.store.model.Product;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Datastore {
- List<String> users = new ArrayList<>();
+ List<Product> products;
+
+ public Datastore() {
+  this.products = new ArrayList<>();
+ }
+
+ public Product create(Product product) {
+  boolean added = products.add(product);
+  return added ? products.get(products.indexOf(product)) : null;
+ }
+
+ public Product findById(Integer id) {
+  Product product = null;
+  for (Product p: products) {
+   if (p.getId() == id) {
+    product = p;
+   }
+  }
+  return product;
+ }
+
+ public List<Product> findAll() {
+  return products;
+ }
+
+ public List<Object> updateById(Integer id, Product p) {
+ }
 }
