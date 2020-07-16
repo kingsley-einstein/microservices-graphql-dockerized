89850d5f9047d4311b82302ba0bd53a304f94735
Kingsley Victor <javaprodigy56@gmail.com>
2020-07-16 10:00:01 -0400

    New commit

---
+++ product-service/src/main/java/com/microservices/playaround/store/Datastore.java
@@ -0,0 +1,8 @@
+package com.microservices.playaround.store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Datastore {
+ List<String> users = new ArrayList<>();
+}
---
+++ product-service/src/main/java/com/microservices/playaround/store/model/Product.java
@@ -0,0 +1,39 @@
+package com.microservices.playaround.store.model;
+
+public class Product implements java.io.Serializable {
+ Integer id;
+ String name;
+ Integer owner;
+ String description;
+
+ public Product(Integer id, String name, Integer owner, String description) {
+  this.id = id;
+  this.name = name;
+  this.owner = owner;
+  this.description = description;
+ }
+
+ public void setDescription(String description) {
+  this.description = description;
+ }
+
+ public String getDescription() {
+  return description;
+ }
+
+ public void setName(String name) {
+  this.name = name;
+ }
+
+ public Integer getId() {
+  return id;
+ }
+
+ public String getName() {
+  return name;
+ }
+
+ public Integer getOwner() {
+  return owner;
+ }
+}
