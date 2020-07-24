bdab4c68081e42e132f6a1654fa468731abe3c76
Kingsley Victor <javaprodigy56@gmail.com>
2020-07-24 16:10:23 -0400

    New commit

--- product-service/src/main/java/com/microservices/playaround/router/Router.java
+++ product-service/src/main/java/com/microservices/playaround/router/Router.java
@@ -3,6 +3,7 @@ package com.microservices.playaround.router;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.microservices.playaround.server.CustomErrorResponse;
 import com.microservices.playaround.server.CustomServerResponse;
 import com.microservices.playaround.service.ProductService;
 import com.microservices.playaround.store.model.Product;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,16 +34,33 @@ public class Router {
 
  @PostMapping("/create")
  public ResponseEntity<CustomServerResponse<Product>> create(@RequestBody Map<String, Object> body) {
-  Map<String, Object> map = new HashMap<>();
-  body.keySet().forEach((key) -> {
-   map.put(key, body.get(key));
-  });
-  // I will remove comments when I integrate with Openfeign
-  // map.put("owner", ownerId);
-  Product product = service.create(map);
-  return new ResponseEntity<>(
-   new CustomServerResponse<Product>(201, product),
-   HttpStatus.CREATED
-  );
+  try {
+   Map<String, Object> map = new HashMap<>();
+    body.keySet().forEach((key) -> {
+     map.put(key, body.get(key));
+    });
+   // I will remove comments when I integrate with Openfeign
+   // map.put("owner", ownerId);
+   Product product = service.create(map);
+   return new ResponseEntity<>(
+    new CustomServerResponse<Product>(201, product),
+    HttpStatus.CREATED
+   );
+  } catch (CustomErrorResponse error) {
+   throw new CustomErrorResponse(error.getCode(), error.getMessage());
+  }
+ }
+
+ @GetMapping("/find/{id}")
+ public ResponseEntity<CustomServerResponse<Product>> getOneById(@PathVariable("id") Integer id) {
+  try {
+   Product product = service.findById(id);
+   return new ResponseEntity<>(
+    new CustomServerResponse<Product>(200, product),
+    HttpStatus.OK
+   );
+  } catch (CustomErrorResponse error) {
+   throw new CustomErrorResponse(error.getCode(), error.getMessage());
+  }
  }
 }
