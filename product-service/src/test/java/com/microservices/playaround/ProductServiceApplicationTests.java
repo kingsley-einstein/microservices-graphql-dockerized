351d7a975733351c849544c6c4829669e4022d00
Kingsley Victor <javaprodigy56@gmail.com>
2020-09-13 08:33:29 +0100

    Setting up tests

--- product-service/src/test/java/com/microservices/playaround/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/microservices/playaround/ProductServiceApplicationTests.java
@@ -3,10 +3,14 @@ package com.microservices.playaround;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class ProductServiceApplicationTests {
 
 	@Test
